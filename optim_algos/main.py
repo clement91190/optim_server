bd2fcb9ec24c8bceecfe395b77ccade9296e1e96
clement91190 <clem.jambou@gmail.com>
2014-02-19 20:50:56 +0000

    merged ...

--- optim_algos/main.py
+++ optim_algos/main.py
@@ -8,16 +8,10 @@ def main():
     optim_problem = "quad_learning"
     optim_run = work_endpoints.get_run_num(optim_problem)
     print optim_problem, optim_run
-<<<<<<< HEAD
-    fitness = lambda tab: endpoints.eval_fitness_function(tab, optim_problem, optim_run)
-    algo = GeneticAlgorithm(fitness)
-    t = Test(algo, (None, 2), 1000)
-=======
     fitness = lambda tab: work_endpoints.eval_fitness_function(tab, optim_problem, optim_run)
-    algo = Simplex(fitness)
-    size = 9  #num of free params
+    algo = GeneticAlgorithm(fitness)
+    size = 9  # num of free params
     t = Test(algo, (None, size), 1000)
->>>>>>> 8121bc2c2d5cb08321b0fe49be0a96d544931c83
     t.optimize()
     
 
